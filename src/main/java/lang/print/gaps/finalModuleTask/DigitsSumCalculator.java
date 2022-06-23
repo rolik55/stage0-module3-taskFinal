457b13867c78cedab791ea3b193733191c1e8449
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-23 13:13:59 +0300

    Last digit fix

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -6,7 +6,7 @@ public class DigitsSumCalculator {
         digit1 = number / 1000;
         digit2 = number / 100 % 10;
         digit3 = number / 10 % 10;
-        digit4 = number % 1000;
+        digit4 = number % 10;
         result = digit1 + digit2 + digit3 + digit4;
         System.out.println(result);
     }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -5,7 +5,7 @@ public class NumberReverter {
         int digit1, digit2, digit3, result;
         digit1 = number / 100;
         digit2 = number / 10 % 10;
-        digit3 = number % 100;
+        digit3 = number % 10;
         result = digit3 * 100 + digit2 * 10 + digit1;
         System.out.println(result);
     }
