2bc81779c5191529f461106d6bea173be2416ba6
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-23 13:08:58 +0300

    Task finished

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int digit1, digit2, digit3, digit4, result;
+        digit1 = number / 1000;
+        digit2 = number / 100 % 10;
+        digit3 = number / 10 % 10;
+        digit4 = number % 1000;
+        result = digit1 + digit2 + digit3 + digit4;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int digit1, digit2, digit3, result;
+        digit1 = number / 100;
+        digit2 = number / 10 % 10;
+        digit3 = number % 100;
+        result = digit3 * 100 + digit2 * 10 + digit1;
+        System.out.println(result);
     }
 }
